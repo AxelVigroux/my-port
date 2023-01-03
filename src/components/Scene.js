014517fa95a13ce20dce14f34d9581f11dccc37e
PeretEtienne <etienne.peret@integragen.com>
2023-01-03 17:01:23 +0100

    Offscreen scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,17 +1,30 @@
-import { useRef, useMemo } from "react";
-import Spline from "@splinetool/react-spline";
+import { useRef } from 'react';
+import Spline from '@splinetool/react-spline';
+import { useLocation } from 'react-router-dom';
 
 export default function Scene() {
   const computer = useRef();
-  const link = "https://prod.spline.design/2DmV0Vd0-e0YFvfo/scene.splinecode";
+  const location = useLocation();
+  const link = 'https://prod.spline.design/2DmV0Vd0-e0YFvfo/scene.splinecode';
 
   function onLoad(spline) {
-    const obj = spline.findObjectByName("Computer");
+    const obj = spline.findObjectByName('Computer');
     computer.current = obj;
   }
 
   return (
-    <div>
+    <div
+      style={
+        location.pathname !== '/'
+          ? {
+              position: 'absolute',
+              top: -99999,
+              width: '100vw',
+              height: '100vh',
+            }
+          : {}
+      }
+    >
       <Spline scene={link} onLoad={onLoad} />
     </div>
   );
